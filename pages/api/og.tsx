8f176c97a5d90bdbdbd40a942c8666e3c9fd2e9f
syu.onoda <s-onoda@creative-innovation.co.jp>
2023-09-25 02:23:29 +0900

    画像の変更

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -26,12 +26,7 @@ export default async function ogp(req: NextRequest) {
       <div
         style={{
           fontSize: 50,
-          backgroundImage: `url(${`data:image/svg+xml,${encodeURIComponent(
-            '<svg height="600" viewBox="0 0 900 600" width="900" xmlns="http://www.w3.org/2000/svg"><path d="m0 0h900v600h-900z" fill="#001829"/><g fill="none" stroke="#297ea6" stroke-width="2"><path d="m0-147 127.3 73.5v147l-127.3 73.5-127.3-73.5v-147z" transform="translate(706 316)"/><path d="m0-131 113.4 65.5v131l-113.4 65.5-113.4-65.5v-131z" transform="translate(141 106)"/><path d="m0-127 110 63.5v127l-110 63.5-110-63.5v-127z" transform="translate(252 581)"/></g></svg>',
-          )}`})`,
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          backgroundSize: "100% 100%",
+          background: "#526D82",
           width: "100%",
           height: "100%",
           display: "flex",
