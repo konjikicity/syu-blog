2291fae9b4c27d396c513bafe477ef49a94ef65e
syu.onoda <s-onoda@creative-innovation.co.jp>
2023-09-26 01:49:57 +0900

    フォントの使用方法を変更

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,108 +1,53 @@
 import { NextRequest } from "next/server";
 import { ImageResponse } from "@vercel/og";
+import OgpComponents from "../../components/ogp-components";
 
 export const config = {
   runtime: "edge",
 };
 
-const font = fetch(
-  new URL("../../public/font/NotoSansJP-Bold.woff", import.meta.url),
-).then((res) => res.arrayBuffer());
-
 export default async function ogp(req: NextRequest) {
   const { searchParams } = new URL(req.url);
-  const fontData = await font;
 
   const hasTitle = searchParams.has("title");
   const title = hasTitle
     ? searchParams.get("title")?.slice(0, 100)
     : "ネトゲ廃人からエンジニアブログ";
 
-  const hasDate = searchParams.has("date");
-  const date = hasDate ? searchParams.get("date") : "";
+  const fontData = await fetchFont(title);
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 50,
-          background: "#526D82",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "white",
-          textAlign: "center",
-          padding: "0px 150px",
-          textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-          position: "relative",
-        }}
-      >
-        <h2
-          style={{
-            width: "100%",
-            color: "white",
-            fontSize: 60,
-            textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-          }}
-        >
-          {title}
-        </h2>
-        <div
-          style={{
-            display: "flex",
-            position: "absolute",
-            width: "100%",
-            bottom: 0,
-            justifyContent: "space-between",
-          }}
-        >
-          <h2
-            style={{
-              color: "white",
-              fontSize: 40,
-              textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-            }}
-          >
-            {date}
-          </h2>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <img
-              src={`https://github.com/konjikicity.png`}
-              alt=""
-              width="60"
-              height="60"
-              style={{ borderRadius: 60, marginRight: 10 }}
-            />
-            <h2
-              style={{
-                color: "white",
-                fontSize: 40,
-                textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-              }}
-            >
-              @konjikicity
-            </h2>
-          </div>
-        </div>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 600,
-      fonts: [
-        {
-          name: "NotoSansJP",
-          data: fontData,
-          style: "normal",
-        },
-      ],
-    },
+  return new ImageResponse(<OgpComponents title={title} />, {
+    width: 1200,
+    height: 600,
+    fonts: [
+      {
+        name: "NotoSansJP",
+        data: fontData,
+        style: "normal",
+      },
+    ],
+  });
+}
+
+export async function fetchFont(text: string): Promise<ArrayBuffer | null> {
+  const googleFontsUrl = `https://fonts.googleapis.com/css2?family=Noto+Sans+JP&text=${encodeURIComponent(
+    text,
+  )}`;
+
+  const css = await (
+    await fetch(googleFontsUrl, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+      },
+    })
+  ).text();
+
+  const resource = css.match(
+    /src: url\((.+)\) format\('(opentype|truetype)'\)/,
   );
+
+  if (!resource) return null;
+  const res = await fetch(resource[1]);
+  return res.arrayBuffer();
 }
